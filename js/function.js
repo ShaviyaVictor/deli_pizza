393daa596bfa79c11604c7eb5f27081dca27b1e0
ShaviyaVictor <shaviyavictor@gmail.com>
2021-11-29 20:43:13 +0300

    Add the business logic of the program

---
+++ js/function.js
@@ -0,0 +1,9 @@
+//    business logic
+
+function Orderpizza(name, size, crust, topping, total){
+  this.name = name;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.total = total;
+}
