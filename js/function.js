083a4beda081354a6b33038ede3e0febb9601f2f
ShaviyaVictor <shaviyavictor@gmail.com>
2021-11-29 20:45:24 +0300

    Add the UI logic and initialize the picking of values from the form inputs

--- js/function.js
+++ js/function.js
@@ -7,3 +7,12 @@ function Orderpizza(name, size, crust, topping, total){
   this.topping = topping;
   this.total = total;
 }
+
+//      UI logic
+//      add functionality to the proceed button
+$(document).ready(function(){
+  $("button.ordernow").click(function(event){
+    let pname = $("name option:selected").val();
+    let psize = $("#size option:selected").val();
+    let pcrust = $("#crust option:selected").val();
+    let ptopping = [];
