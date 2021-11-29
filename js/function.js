4e17a22d64fe72149f5cec7acdbed5a8e61aa6dc
ShaviyaVictor <shaviyavictor@gmail.com>
2021-11-29 20:47:45 +0300

    Add the switch points for the values declared by the user

--- js/function.js
+++ js/function.js
@@ -16,3 +16,37 @@ $(document).ready(function(){
     let psize = $("#size option:selected").val();
     let pcrust = $("#crust option:selected").val();
     let ptopping = [];
+
+    $.each($("input[name='toppings']:checked"), 
+    function(){            
+      ptopping.push($(this).val());
+     });
+
+  switch(psize){
+    case "0":
+      price =0;
+    break;
+    case "small":
+      price = 500;
+    break;
+    case "medium":
+      price = 1000;
+    break;
+    case "large":
+       price = 1500;
+    break;
+  }
+  switch(pcrust){
+    case "0":
+      crust_price = 0;
+    break;
+    case "Stuffed":
+      crust_price = 100;
+    break;
+    case "Cracker":
+      crust_price = 200;
+    break;
+    case "Thin":
+      crust_price = 300;
+    break;
+  }
